7efebc81488aa0b131ab9f9f19c4a7ae44fc0b6d
Giulio Falco <gfalco58@gmail.com>
2022-05-23 14:16:14 +0200

    migrazione cambiamenti database

---
+++ pcto/dati/csvJson.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import csv, json
+
+csvFilePath = "tutor.csv"
+jsonFilePath = 'tutor.json'
+
+data = {}
+with open(csvFilePath) as csvFile:
+   csvReader = csv.DictReader(csvFile)
+   for rows in csvReader:
+      id = rows['id']
+      data[id] = rows
+      
+with open(jsonFilePath,'w') as jsonFile:
+      jsonFile.write(json.dumps(data, indent=4))
