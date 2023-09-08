49782e4b30da773afbb10e7b95118123dea5c10e
Giulio Falco <gfalco58@gmail.com>
2023-09-09 01:18:31 +0200

    modificato il model

--- festival/models.py
+++ festival/models.py
@@ -29,6 +29,7 @@ class Risposte(models.Model):
     domanda = models.ForeignKey(Domande,on_delete=models.CASCADE)
     risposta = models.TextField(null=True,blank=True)
     questionario = models.ForeignKey(Questionari,on_delete=models.CASCADE)
+    valutaione = models.IntegerField(blank=True,null=True)
 
     def __str__(self):
         return (f"{self.questionario.id} {self.domanda} {self.risposta}")
