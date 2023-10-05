10c7c5b838b8354cd42ced07fb30b3ac1693f017
Giulio Falco <gfalco58@gmail.com>
2023-10-06 00:54:55 +0200

    upload csv

--- tutor/views.py
+++ tutor/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from pcto.models import *
+from tutor.models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, HttpResponseRedirect
@@ -44,3 +44,47 @@ def upload(request):
 def upload_csv_proposte(request):
    # carica il csv delle proposte
    return(HttpResponse("<h2>Dati caricati con successo</h2> <a href='/pcto/tutor'>Torna alla hoem page</a>"))
+
+import csv
+from django.db import IntegrityError
+def upload_csv_proposte(request):
+    # carica dal file csv l'elenco aggiornato delle aziende
+
+   if request.method == 'POST':
+         file_csv = request.FILES['archivio'] 
+         file_content = file_csv.read().decode('utf-8').splitlines()
+        
+         reader = csv.DictReader(file_content,delimiter=';')
+        
+         # aziende = Aziende.objects.all()
+         #for az in aziende:
+         #    az.delete()
+
+         errori = []
+         for row in reader:
+               
+               proposta = Proposte (
+                    nome_progetto = row['nome_progetto'],
+                    disciplina =row['disciplina'],
+                    max_alunni = row['max_alunni'],
+                    classi = row['classi'],
+                    ente = row['ente'],
+                    referente_esterno = row['referente_esterno'],
+                    email_referente_esterno = row['email_referente_esterno'],
+                    tel_referente_esterno = row['tel_referente_esterno'],
+                    num_ore = row['num_ore'],
+                    data_inizio = row['data_inizio'],
+                    data_fine = row['data_fine'],
+               )
+               try:
+                  proposta.save()
+               except IntegrityError as e:
+                   errori.append(f"errore in {row['nome_progetto']} {e}")
+               except Exception as e:
+                    errori.append(f"errore sconosciuto in {row['nome_progetto']} {e}")
+                    
+         if errori:
+                context = {'errori':errori}
+                return render(request,"errori_importazione.html",context)
+           
+   return render(request,"errori_importazione.html",{})  
