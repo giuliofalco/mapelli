36618c21946d6eb7f2f1891885c10c76cc7e98d5
Giulio Falco <gfalco58@gmail.com>
2023-10-05 21:08:08 +0200

    cambio ordine url

--- mapelli/urls.py
+++ mapelli/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='pcto/', permanent=False)),
-    path('pcto/',include('pcto.urls')),
     path('festival/',include('festival.urls')),
     path('orientamento/',include('orientamento.urls')),
+    path('pcto/',include('pcto.urls')),
 ]
