7e32b154fed7cbc1d8c9303c2b367078ba9349bc
Giulio Falco <gfalco58@gmail.com>
2023-10-06 14:57:32 +0200

    modificato filter x adeguarlo ai nuovi campi

--- pcto/filters.py
+++ pcto/filters.py
@@ -4,9 +4,9 @@ from .models import *
 
 class AziendeFilter(django_filters.FilterSet):
     ragione_sociale = CharFilter(field_name="ragione_sociale",lookup_expr="icontains")
-    sede_comune = CharFilter(field_name="sede_comune",lookup_expr="icontains")
-    sede_provincia = CharFilter(field_name="sede_provincia",lookup_expr="icontains")
-    settore = CharFilter(field_name="settore",lookup_expr="icontains")
+    comune = CharFilter(field_name="comune",lookup_expr="icontains")
+    provincia = CharFilter(field_name="provincia",lookup_expr="icontains")
+    codice_ateco = CharFilter(field_name="codice_ateco",lookup_expr="icontains")
     class Meta:
         model = Aziende
         fields = []
