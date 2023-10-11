95cd90c61acfed92d527ce6d37ec7600cfd9207d
Giulio Falco <gfalco58@gmail.com>
2023-10-11 18:06:29 +0200

    tolto / da pcto/ url.py Mapelli

--- mapelli/urls.py
+++ mapelli/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pcto/tutor/',include('tutor.urls')),
     path('', RedirectView.as_view(url='pcto', permanent=False)),
-    path('pcto/',include('pcto.urls')),
+    path('pcto',include('pcto.urls')),
     path('festival/',include('festival.urls')),
      
 ]
