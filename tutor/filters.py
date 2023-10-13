9b756274ac6f544d8a3e52e48d232b537cd59d0f
Giulio Falco <gfalco58@gmail.com>
2023-10-13 09:02:41 +0200

    label in filter.py

--- tutor/filters.py
+++ tutor/filters.py
@@ -3,10 +3,10 @@ from django_filters import CharFilter, ModelChoiceFilter
 from .models import *
 
 class ProposteFilter(django_filters.FilterSet):
-    nome_progetto = CharFilter(field_name="nome_progetto",lookup_expr="icontains")
-    ente = CharFilter(field_name="ente",lookup_expr="icontains")
-    indirizzo = ModelChoiceFilter(field_name='indirizzo',queryset=Indirizzi.objects.all())
-    tipologia = ModelChoiceFilter(field_name='tipologia',queryset=Tipologie_proposte.objects.all())
+    nome_progetto = CharFilter(field_name="nome_progetto",lookup_expr="icontains",label='Proposta ')
+    ente = CharFilter(field_name="ente",lookup_expr="icontains",label='Ente ')
+    indirizzo = ModelChoiceFilter(field_name='indirizzo',queryset=Indirizzi.objects.all(),label='Indirizzo ')
+    tipologia = ModelChoiceFilter(field_name='tipologia',queryset=Tipologie_proposte.objects.all(),label='Tipologia ')
     
     class Meta:
         model = Proposte
