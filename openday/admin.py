d0f0d4fdb1c7063e4e2078a56d43e75ecb42d03e
Giulio Falco <gfalco58@gmail.com>
2023-10-17 20:51:02 +0200

    admin

--- openday/admin.py
+++ openday/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['titolo','autore','data']
