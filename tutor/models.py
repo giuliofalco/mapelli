da4ade740c7212e47b6567295bde904622d01179
Giulio Falco <gfalco58@gmail.com>
2023-10-17 06:09:35 +0200

    model tutor richtext e migration

--- tutor/models.py
+++ tutor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 
 class Tipologie_proposte (models.Model):
    # tipologie di proposte
@@ -98,7 +99,7 @@ class News(models.Model):
     autore = models.CharField(max_length=100,blank=True,null=True,default='admin')
     data = models.DateTimeField(blank=True,null=True,default=timezone.now)
     titolo = models.CharField(max_length=100,null=True,blank=True,default='')
-    testo = models.TextField()
+    testo = RichTextField()
 
     def __str__(self):
        return(self.titolo)
