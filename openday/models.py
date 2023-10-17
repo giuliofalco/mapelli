eb7472979de5754b26d261c56fb3d211dab6933b
Giulio Falco <gfalco58@gmail.com>
2023-10-17 20:47:18 +0200

    news in openday

--- openday/models.py
+++ openday/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class News(models.Model):
+    autore = models.CharField(max_length=100,blank=True,null=True,default='admin')
+    data = models.DateTimeField(blank=True,null=True,default=timezone.now)
+    titolo = models.CharField(max_length=100,null=True,blank=True,default='')
+    testo = RichTextField()
+
+    def __str__(self):
+       return(self.titolo)
+    
+    class Meta:
+        ordering = ['-data']
