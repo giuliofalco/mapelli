049972331d425d6fce99456646ccefc6df3e7d30
Giulio Falco <gfalco58@gmail.com>
2023-10-31 21:57:04 +0100

    messaggi di errore in italiano

--- openday/forms.py
+++ openday/forms.py
@@ -6,3 +6,17 @@ class IscrizioniForm(ModelForm):
        model = Visitatori
        fields = ['cognome','nome','email','comune','scuola']
        labels = {'comune': 'Comune di provenienza', 'scuola': 'Scuola di provenienza'}
+       error_messages = {
+            'cognome': {
+                'required': "Campo obbligatorio.",
+                'invalid': "Valore non valido.",
+            },
+            'nome': {
+                'required': "Campo obbligatorio.",
+                'invalid': "Valore non valido.",
+            },
+             'comune': {
+                'required': "Campo obbligatorio.",
+                'invalid': "Valore non valido.",
+            }
+        }
