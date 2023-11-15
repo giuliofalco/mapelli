8af946552c1c5343f33b33d68022aeb0f1013d84
Giulio Falco <gfalco58@gmail.com>
2023-11-15 17:46:17 +0100

    corretto il filter su ente

--- tutor/filters.py
+++ tutor/filters.py
@@ -3,16 +3,18 @@ from django_filters import CharFilter, ModelChoiceFilter
 from .models import *
 from django import forms
 
+class EnteSelectWidget(forms.Select):
+    def __init__(self, *args, **kwargs):
+        super(EnteSelectWidget, self).__init__(*args, **kwargs)
+        # Recupera le voci distinte dal campo 'ente' del modello
+        ente_choices = Proposte.objects.values_list('ente', flat=True).distinct().order_by('ente')
+        # Crea le opzioni per il widget
+        self.choices = [(value, value) for value in ente_choices]
+
 class ProposteFilter(django_filters.FilterSet):
     nome_progetto = CharFilter(field_name="nome_progetto",lookup_expr="icontains",label='Proposta ')
-    ente = CharFilter(field_name="ente",lookup_expr="icontains",label='Ente ')
-    #ente = django_filters.ModelChoiceFilter(
-    #    #queryset=Proposte.objects.all().order_by('ente').distinct('ente'),
-    #    queryset=Proposte.objects.values_list('ente', flat=True).distinct().order_by('ente'),
-    #    empty_label="Tutti",  # Etichetta per l'opzione vuota
-    #    label='Ente',
-    #    widget=forms.Select
-    #)
+    ente = CharFilter(field_name="ente",lookup_expr="icontains",label='Ente ',widget=EnteSelectWidget)
+  
     indirizzo = ModelChoiceFilter(field_name='indirizzo',queryset=Indirizzi.objects.all(),label='Indirizzo ')
     tipologia = ModelChoiceFilter(field_name='tipologia',queryset=Tipologie_proposte.objects.all(),label='Tipologia ')
     
