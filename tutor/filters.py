4b3a0b5852ecc69100b4a3356569e568f58ec0bf
Giulio Falco <gfalco58@gmail.com>
2023-11-15 17:38:29 +0100

    rimesso il filtro su ente come testo

--- tutor/filters.py
+++ tutor/filters.py
@@ -5,14 +5,14 @@ from django import forms
 
 class ProposteFilter(django_filters.FilterSet):
     nome_progetto = CharFilter(field_name="nome_progetto",lookup_expr="icontains",label='Proposta ')
-    # ente = CharFilter(field_name="ente",lookup_expr="icontains",label='Ente ')
-    ente = django_filters.ModelChoiceFilter(
-        #queryset=Proposte.objects.all().order_by('ente').distinct('ente'),
-        queryset=Proposte.objects.values_list('ente', flat=True).distinct().order_by('ente'),
-        empty_label="Tutti",  # Etichetta per l'opzione vuota
-        label='Ente',
-        widget=forms.Select
-    )
+    ente = CharFilter(field_name="ente",lookup_expr="icontains",label='Ente ')
+    #ente = django_filters.ModelChoiceFilter(
+    #    #queryset=Proposte.objects.all().order_by('ente').distinct('ente'),
+    #    queryset=Proposte.objects.values_list('ente', flat=True).distinct().order_by('ente'),
+    #    empty_label="Tutti",  # Etichetta per l'opzione vuota
+    #    label='Ente',
+    #    widget=forms.Select
+    #)
     indirizzo = ModelChoiceFilter(field_name='indirizzo',queryset=Indirizzi.objects.all(),label='Indirizzo ')
     tipologia = ModelChoiceFilter(field_name='tipologia',queryset=Tipologie_proposte.objects.all(),label='Tipologia ')
     
