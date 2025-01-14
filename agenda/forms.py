edfab57c761c631a9b9960cb569483fdcd8469f3
Giulio Falco <gfalco58@gmail.com>
2025-01-14 16:05:56 +0100

    ckeditor

--- agenda/forms.py
+++ agenda/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from .models import *
 
+from ckeditor.widgets import CKEditorWidget
 class DayEntryForm(forms.ModelForm):
     class Meta:
         model = DayEntry
         fields = ['assenze', 'eventi','uscite','note']
         widgets = {
-            'assenze': forms.Textarea(attrs={'rows': 28, 'cols': 10}),
-            'eventi': forms.Textarea(attrs={'rows': 28, 'cols': 10}),
-            'uscite': forms.Textarea(attrs={'rows': 28, 'cols': 10}),
-            'note': forms.Textarea(attrs={'rows': 28, 'cols': 10}),
+            'assenze': CKEditorWidget(),
+            'eventi': CKEditorWidget(),
+            'uscite': CKEditorWidget(),
+            'note':CKEditorWidget(),
         }
--- mapelli/settings.py
+++ mapelli/settings.py
@@ -149,3 +149,30 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+CKEDITOR_CONFIGS = {
+    'default': {
+        #'toolbar': 'full',
+        'toolbar': [
+            ['Format'],
+            ['Bold', 'Italic', 'Underline', 'Strike'],
+            ['Font','FontSize','NumberedList', 'BulletedList'],
+            ['Link','Unlink','TextColor','BGColor'],
+            ['Copy','Cut','Paste'],
+        ],
+
+        'height': 500,
+        'width': '100%',
+        'extraAllowedContent': 'img[width,height]',
+        'extraPlugins': ','.join(['uploadimage', 'uploadwidget',]),
+        'filebrowserBrowseUrl': '/ckeditor/browse/',
+        'filebrowserImageBrowseUrl': '/ckeditor/browse/',
+        'filebrowserUploadUrl': '/ckeditor/upload/',
+        'filebrowserImageUploadUrl': '/ckeditor/upload/',
+        'imageUploadUrl': '/ckeditor/uploads/'
+    }
+}
