7309f2e38bbd936209a845c7a7f32ea75296566d
Giulio Falco <gfalco58@gmail.com>
2025-09-26 22:21:34 +0200

    importato redirect

--- tutor/views.py
+++ tutor/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from tutor.models import *
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout
+
 import csv
 from django.db import IntegrityError
 from django.contrib.auth.views import PasswordChangeView
